8669bde708035e4f694e2e7bf5b5129ffb0a834b
HelenaLC <helena@crowells.eu>
2022-11-28 18:50:12 +0100

    init

---
+++ code/utils.R
@@ -0,0 +1,24 @@
+suppressPackageStartupMessages({
+  library(spatstat.core)
+  library(spatstat.geom)
+  library(SpatialExperiment)
+})
+
+.ppp <- \(spe, marks = NULL) {
+  xy <- spatialCoords(spe)
+  w <- owin(range(xy[, 1]), range(xy[, 2]))
+  m <- if (is.character(marks)) {
+    stopifnot(
+      length(marks) == 1,
+      marks %in% c(
+        gs <- rownames(spe),
+        cd <- names(colData(spe))))
+    if (marks %in% gs) {
+      assay(spe)[marks, ]
+    } else if (marks %in% cd) {
+      spe[[marks]]
+    } else stop("'marks' should be in ",
+      "'rownames(.)' or 'names(colData(.))'")
+  }
+  ppp(xy[, 1], xy[, 2], window = w, marks = m)
+}

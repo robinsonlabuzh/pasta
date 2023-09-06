87d7f7a341cd489ed8cc55d4ae7dc587a2c12c42
sgunz <samuel.gunz@uzh.ch>
2023-09-06 11:14:45 +0200

    updated the relrisk hotspot plots and function

--- code/utils.R
+++ code/utils.R
@@ -33,20 +33,41 @@ suppressPackageStartupMessages({
   ppp(xy[, 1], xy[, 2], window = w, marks = m)
 }
 
-#Code source: https://idblr.rbind.io/post/pvalues-spatial-segregation/
-pvaluesPlot <- function(f1){
+
+pValuesHotspotMarks <- function(pp, alpha = 0.05){
+  # Code source: https://idblr.rbind.io/post/pvalues-spatial-segregation/
+  
   # Significant p-values assumming normality of the Poisson process
   ## relrisk() computes standard errors based on asymptotic theory, assuming a Poisson process
-    alpha <- 0.05                           # alpha
-    z <- qnorm(alpha/2, lower.tail = F)     # z-statistic
-    f1$u <- f1$estimate + z*f1$SE           # Upper CIs
-    f1$l <- f1$estimate - z*f1$SE           # Lower CIs
-    mu_0 <- as.vector(table(spatstat.geom::marks(pp))/pp$n) # null expectations by type
-    f1$p <- f1$estimate # copy structure of pixels, replace values
-    for (i in 1:length(f1$p)) {
-      f1$p[[i]]$v <- factor(ifelse(mu_0[i] > f1$u[[i]]$v, "lower",
-                                  ifelse( mu_0[i] < f1$l[[i]]$v, "higher", "none")),
-                            levels = c("lower", "none", "higher"))
-    }
-    return(f1)
+  # call relative risk function
+  f1 <- relrisk(pp_sel,se=TRUE)
+  
+  z <- qnorm(alpha/2, lower.tail = F)     # z-statistic
+  f1$u <- f1$estimate + z*f1$SE           # Upper CIs
+  f1$l <- f1$estimate - z*f1$SE           # Lower CIs
+  mu_0 <- as.vector(table(spatstat.geom::marks(pp))/pp$n) # null expectations by type
+  f1$p <- f1$estimate # copy structure of pixels, replace values
+  for (i in 1:length(f1$p)) {
+    f1$p[[i]]$v <- factor(ifelse(mu_0[i] > f1$u[[i]]$v, "lower",
+                                ifelse( mu_0[i] < f1$l[[i]]$v, "higher", "none")),
+                          levels = c("lower", "none", "higher"))
+  }
+  return(f1)
+}
+
+pValuesHotspot <- function(pp, alpha = 0.05){
+  # Code source: https://idblr.rbind.io/post/pvalues-spatial-segregation/
+  # density estimate for all marks
+  f1 <- density(unmark(pp), se = TRUE)
+  # Significant p-values assumming normality of the Poisson process
+  z <- qnorm(alpha/2, lower.tail = F)     # z-statistic
+  f1$u <- f1$estimate + z*f1$SE           # Upper CIs
+  f1$l <- f1$estimate - z*f1$SE           # Lower CIs
+  mu_0 <- intensity(unmark(pp)) # null expectations by type
+  f1$p <- f1$estimate # copy structure of pixels, replace values
+  f1$p$v <- factor(ifelse(mu_0 > f1$u$v, "lower",
+                          ifelse( mu_0 < f1$l$v, "higher", "none")),
+                   levels = c("lower", "none", "higher"))
+  
+  return(f1)
 }
